37c5a940a8ef2c29ae01aba1433db25ba5792eaf
alexiosgrig <alexios.grig@gmail.com>
2025-10-24 22:26:59 +0300

    Add constants.

---
+++ frontend/src/constants/healthMenuItemList.ts
@@ -0,0 +1,7 @@
+export const healthMenuItemList = [
+  { value: "", label: "All" },
+  { value: "health", label: "Health" },
+  { value: "good", label: "Good" },
+  { value: "moderate", label: "Moderate" },
+  { value: "critical", label: "Critical" },
+];
