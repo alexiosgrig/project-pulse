60b3b145e16a4c0601579ee753c8f226b8cfda8c
alexiosgrig <alexios.grig@gmail.com>
2025-10-24 22:27:59 +0300

    Add TopAppBar.tsx with types

---
+++ frontend/src/types/props/TopAppBarProps.ts
@@ -0,0 +1,9 @@
+import type { Dispatch, SetStateAction } from "react";
+
+export interface TopAppBarProps {
+  bulkMode: boolean;
+  setBulkMode: Dispatch<SetStateAction<boolean>>;
+  setOpenAddDialog: Dispatch<SetStateAction<boolean>>;
+  handleBulkDelete: any;
+  selectedIds: number[];
+}
