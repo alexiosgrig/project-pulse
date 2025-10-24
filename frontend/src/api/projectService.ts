16903cff34297c72250b86bc52f3690f95329288
alexiosgrig <alexios.grig@gmail.com>
2025-10-25 02:17:07 +0300

    Change api url

--- frontend/src/api/projectService.ts
+++ frontend/src/api/projectService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { ProjectItem } from "../types/ProjectItem";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api/projects", // change to your backend URL
+  baseURL: "http://127.0.0.1:8000/api/projects", // change to your backend URL
   headers: { "Content-Type": "application/json" },
 });
 
@@ -11,12 +11,16 @@ export const fetchProjects = async (): Promise<ProjectItem[]> => {
   return response.data;
 };
 
-export const addProject = async (project: ProjectItem): Promise<ProjectItem> => {
+export const addProject = async (
+  project: ProjectItem
+): Promise<ProjectItem> => {
   const response = await api.post("/", project);
   return response.data;
 };
 
-export const updateProject = async (project: ProjectItem): Promise<ProjectItem> => {
+export const updateProject = async (
+  project: ProjectItem
+): Promise<ProjectItem> => {
   const response = await api.put(`/${project.id}`, project, {
     headers: { "If-Match": project.version.toString() }, // optimistic concurrency
   });
