ba7ac3baeb323f3d853335ca7b79e27dc93fcebc
alexiosgrig <alexios.grig@gmail.com>
2025-10-26 00:55:40 +0300

    Refactor baseUrl to support elastic search.

--- frontend/src/api/projectService.ts
+++ frontend/src/api/projectService.ts
@@ -4,14 +4,14 @@ import type { PaginatedProjectsResponse } from "../types/PaginatedProjectsRespon
 import type { FetchProjectsParams } from "../types/FetchProjectsParams";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/projects", // change to your backend URL
+  baseURL: "http://127.0.0.1:8000/api/", // change to your backend URL
   headers: { "Content-Type": "application/json" },
 });
 
 export const fetchProjects = async (
   params: FetchProjectsParams
 ): Promise<PaginatedProjectsResponse> => {
-  const response = await api.get("/", {
+  const response = await api.get("/projects", {
     params,
   });
   return response.data;
@@ -20,21 +20,21 @@ export const fetchProjects = async (
 export const fetchProjectsByPage = async (
   params: FetchProjectsParams
 ): Promise<PaginatedProjectsResponse> => {
-  const response = await api.get("/", { params });
+  const response = await api.get("/projects", { params });
   return response.data;
 };
 
 export const addProject = async (
   project: ProjectItem
 ): Promise<ProjectItem> => {
-  const response = await api.post("/", project);
+  const response = await api.post("/projects", project);
   return response.data;
 };
 
 export const updateProject = async (
   project: ProjectItem
 ): Promise<ProjectItem> => {
-  const response = await api.put(`/${project.id}`, project, {
+  const response = await api.put(`/projects/${project.id}`, project, {
     headers: { "If-Match": project.version.toString() },
   });
   return response.data;
