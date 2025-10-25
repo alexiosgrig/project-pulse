aa37ecceed551937fe40fd1cc6885be57b36a7e2
alexiosgrig <alexios.grig@gmail.com>
2025-10-25 23:16:47 +0300

    SearchComponent.tsx Initial implementation

---
+++ frontend/src/components/search-component/SearchComponent.tsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import {
+  TextField,
+  List,
+  ListItem,
+  ListItemText,
+  InputAdornment,
+} from "@mui/material";
+import { Search } from "@mui/icons-material";
+import { searchProject } from "../../api/projectService";
+import type { SearchComponentProps } from "../../types/props/SearchComponentProps";
+
+export const SearchComponent: React.FC<SearchComponentProps> = ({
+  setProjects,
+}) => {
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const data = await searchProject(query);
+        setProjects(data.results);
+      } catch (err) {
+        console.warn(err);
+      }
+    };
+
+    if (query.trim() !== "") {
+      fetchProjects();
+    } else {
+      setProjects([]); // clear when query empty
+    }
+  }, [query]);
+
+  return (
+    <div style={{ maxWidth: 500, margin: "20px auto" }}>
+      <TextField
+        fullWidth
+        label="Search projects..."
+        variant="outlined"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Search />
+            </InputAdornment>
+          ),
+        }}
+      />
+      {/* 
+      <List>
+        <ListItem divider>
+          <ListItemText primary={"dw"} secondary={"dwdw"} />
+        </ListItem>
+      </List> */}
+    </div>
+  );
+};
---
+++ frontend/src/types/props/SearchComponentProps.ts
@@ -0,0 +1,5 @@
+import type { Dispatch, SetStateAction } from "react";
+
+export interface SearchComponentProps {
+  setProjects: Dispatch<SetStateAction<any>>;
+}
